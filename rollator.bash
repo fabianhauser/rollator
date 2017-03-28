cd965e71dbd6edf80825a3f4d236eeb9866e65dc
Fabian Hauser <fabian@fh2.ch>
2017-03-28 16:20:27 +0200

    Add separate lockfile

--- rollator.bash
+++ rollator.bash
@@ -2,15 +2,16 @@
 set -e
 
 WATCH_FILE=$1
-IMAGE=$2
-SERVICE=$3
+LOCK_FILE=$2
+IMAGE=$3
+SERVICE=$4
 
 while true; do
   # Wait on a change on the watchfile
   inotifywait -e create ${WATCH_FILE}
 
   echo Waiting on lock release
-  if [ `flock -n ${WATCH_FILE}` ]; then
+  if [ `flock -n $LOCK_FILE}` ]; then
     echo Lock aquired
     while rm touchfile 2>/dev/null; do 
       OLD_IMAGE_HASH=`docker images ${IMAGE} -q`
@@ -21,7 +22,7 @@ while true; do
         systemctl restart ${SERVICE}
       fi
     done
-    flock -u ${WATCH_FILE}
+    flock -u ${LOCK_FILE}
     echo Lock freed
   else
     echo Deployment currently running
