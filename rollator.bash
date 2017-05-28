13c894873027f93776c2aaafb11ef6b197294ca8
Fabian Hauser <fabian@fh2.ch>
2017-05-28 13:04:12 +0200

    Remove locking mechanism from script

--- rollator.bash
+++ rollator.bash
@@ -8,26 +8,22 @@ IMAGE=$3
 SERVICE=$4
 
 while true; do
-  echo "Wait on a change on the watchfile"
+  echo "Wait on a watchfile change"
   inotifywait -e create ${WATCH_DIR}
 
-  echo "Waiting on lock release"
-  ( flock -n 9 || exit 1
-    echo "Lock aquired"
-    while rm ${WATCH_FILE} 2>/dev/null; do 
-      OLD_IMAGE_HASH=`docker images ${IMAGE} -q`
+  while rm ${WATCH_FILE} 2>/dev/null; do 
+    echo "Doing rollover"
+    OLD_IMAGE_HASH=`docker images ${IMAGE} -q`
 
-      echo "Pulling container"
-      docker pull ${IMAGE}
+    echo "Pulling container"
+    docker pull ${IMAGE}
   
-      if [ "`docker images -q ${IMAGE}`" != "${OLD_IMAGE_HASH}" ]; then
-        echo "Restarting container"
-        systemctl restart ${SERVICE}
-      else
-        echo "No restart required (hashes match)"
-      fi
-    done
-    echo "Lock freed"
-  ) 9>$LOCK_FILE
-  echo "Continue"
+    if [ "`docker images -q ${IMAGE}`" != "${OLD_IMAGE_HASH}" ]; then
+      echo "Restarting container"
+      systemctl restart ${SERVICE}
+    else
+      echo "No restart required (hashes match)"
+    fi
+    echo "Rollover finished"
+  done
 done
